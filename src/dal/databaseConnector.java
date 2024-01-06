7ad8e3c8093e02c421f67036c8312815222e0b88
Matej Grubišić <grubisic401@gmail.com>
2024-01-06 16:30:53 +0100

    Database connection done

--- src/dal/databaseConnector.java
+++ src/dal/databaseConnector.java
@@ -1,8 +1,16 @@
 package dal;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 public class databaseConnector {
+    private static final String JDBC_URL = "jdbc:sqlserver://10.176.111.34\\EASV-DB4:1433;databaseName=mymovie_blabla;trustServerCertificate=true";
+    private static final String USER = "CSe2023b_e_6";
+    private static final String PASSWORD = "CSe2023bE6#23";
 
+    public static Connection getConn() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+    }
 }
