47d106eda436a5239d0cfce02459341000fd6611
alekss <anoviconoks@gmail.com>
2024-01-06 18:02:02 +0200

    Added file choosing button to NewMovie.fxml + filled imdb and personal rating categoryboxes with scores from 1-10.

--- src/gui/NewMovieController.java
+++ src/gui/NewMovieController.java
@@ -1,19 +1,33 @@
 package gui;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class NewMovieController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NewMovieController implements Initializable {
     public Button savebtn;
     public Button cancelNewMovie;
     public TextField titlelbl;
     public TextField lengthlbl;
-    public ChoiceBox imdb;
-    public ChoiceBox personalR;
+    public ChoiceBox<String> imdb;
+    public String[] imdbrating={"1","2","3","4","5","6","7","8","9","10"};
+    public ChoiceBox<String> personalR;
+    public String[] personalRating={"1","2","3","4","5","6","7","8","9","10"};
+    public Button filechoosebtn;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        imdb.getItems().addAll(imdbrating);
+        personalR.getItems().addAll(personalRating);
+    }
+
 
     public void saveMovie(ActionEvent actionEvent) {
     }
@@ -21,5 +35,8 @@ public class NewMovieController {
     public void cancelMovie(ActionEvent actionEvent) {
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         stage.close();
+    }
+
+    public void chooseFile(ActionEvent actionEvent) {
     }
 }
