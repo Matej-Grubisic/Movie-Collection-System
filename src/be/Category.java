35e6c7ef2eb1b2318dbbe0a0b4749469fad5defa
alekss <anoviconoks@gmail.com>
2024-01-06 16:44:28 +0200

    Created Movie and Category classes with getters,setters and constructor.

--- src/be/Category.java
+++ src/be/Category.java
@@ -1,4 +1,44 @@
 package be;
 
+import java.util.ArrayList;
+
 public class Category {
+    private String name;
+    private ArrayList<Movie> allMovies;
+    private int id;
+
+    public Category(String name, ArrayList<Movie> allMovies) {
+        this.name = name;
+        this.allMovies = allMovies;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Movie> getAllMovies() {
+        return allMovies;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAllMovies(ArrayList<Movie> allMovies) {
+        this.allMovies = allMovies;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void removeMovies(Movie movie){
+        allMovies.remove(movie);
+    }
+    public void getMovieCount(){
+        allMovies.size();
+    }
 }
--- src/be/Movie.java
+++ src/be/Movie.java
@@ -1,4 +1,75 @@
 package be;
 
 public class Movie {
+
+    private String movieTitle;
+    private double movieLength;
+    private int imdbRating;
+    private int persRating;
+    private String filepath;
+    private int id;
+
+    public Movie(String movieTitle, double movieLength, int imdbRating, int persRating, String filepath) {
+        this.movieTitle = movieTitle;
+        this.movieLength = movieLength;
+        this.imdbRating = imdbRating;
+        this.persRating = persRating;
+        this.filepath = filepath;
+
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public void setMovieLength(double movieLength) {
+        this.movieLength = movieLength;
+    }
+
+    public void setImdbRating(int imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+    public void setPersRating(int persRating) {
+        this.persRating = persRating;
+    }
+
+    public void setFilepath(String filepath) {
+        this.filepath = filepath;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public double getMovieLength() {
+        return movieLength;
+    }
+
+    public int getImdbRating() {
+        return imdbRating;
+    }
+
+    public int getPersRating() {
+        return persRating;
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getMovieLengthString() {
+
+        long minutes = (long) ((movieLength % 3600) / 60);
+        long seconds = (long) (movieLength % 60);
+
+        return String.format("%02d:%02d", minutes, seconds);
+    }
 }
